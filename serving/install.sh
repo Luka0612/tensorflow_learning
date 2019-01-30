255f43fb017f35b2a293b196f91ae6657ccebf66
xuxiaolong <453935413@qq.com>
2019-01-30 18:10:34 +0800

    采用docker安装tf-server

--- serving/install.sh
+++ serving/install.sh
@@ -11,3 +11,4 @@ bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mavx2 --
 # https://my.oschina.net/jxcdwangtao/blog/1585363 相关参数解释
 # https://github.com/tensorflow/serving/issues/344 参数调优
 # https://bbs.aliyun.com/read/563646.html?spm=5176.10695662.1996646101.searchclickresult.49074655qy58u0 阿里云集群部署
+# https://blog.csdn.net/y19930105/article/details/80763467 采用docker安装，推荐！
